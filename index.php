f5d7b2dc184166923958c924599b56be2296a160
christophesd <christophe.sd@gmail.com>
2020-03-13 16:32:35 +0100

    day1

--- index.php
+++ index.php
@@ -4,9 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simplon Voyage</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,800|Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <hr>
+
+    <nav class="navbar">
+        <h1 class="navbar-title">Simplon Voyage</h1>
+        <label class="navbar-burger" for="toggle">
+            <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.888889 13H15.1111C15.6 13 16 12.5125 16 11.9167C16 11.3208 15.6 10.8333 15.1111 10.8333H0.888889C0.4 10.8333 0 11.3208 0 11.9167C0 12.5125 0.4 13 0.888889 13ZM0.888889 7.58333H15.1111C15.6 7.58333 16 7.09583 16 6.5C16 5.90417 15.6 5.41667 15.1111 5.41667H0.888889C0.4 5.41667 0 5.90417 0 6.5C0 7.09583 0.4 7.58333 0.888889 7.58333ZM0 1.08333C0 1.67917 0.4 2.16667 0.888889 2.16667H15.1111C15.6 2.16667 16 1.67917 16 1.08333C16 0.4875 15.6 0 15.1111 0H0.888889C0.4 0 0 0.4875 0 1.08333Z" fill="#1A7D9F"/>
+            </svg>
+        </label>
+        <input type="checkbox" id="toggle" />
+        <ul class="navbar-menu">
+            <li>
+                <a href="#">Nos séjours</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
 </body>
+
 </html>
