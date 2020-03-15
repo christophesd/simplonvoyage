9fe031fba35dc4c3d475beb57da815da41be69ec
christophesd <christophe.sd@gmail.com>
2020-03-15 22:12:32 +0100

    page produit terminé sans réponsive

--- index.php
+++ index.php
@@ -20,84 +20,86 @@
     
 
     <main id="main">
-        <h2>Nos derniers séjours :</h2>
-        <div class="cards">
-            <div class="card">
-                <img src="img/destination/001/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+        <div class="sejours">
+            <h2>Nos derniers séjours :</h2>
+            <div class="cards">
+                <div class="card">
+                    <img src="img/destination/001/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/002/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/003/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/004/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/005/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/006/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/007/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/008/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/destination/009/main.jpg" alt="" class="card-img">
+                    <div class="card-content">
+                        <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="card-content-price">à partire de 684€</span>
+                        <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
+                    </div>
                 </div>
             </div>
-            <div class="card">
-                <img src="img/destination/002/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
+            <div class="linkpage">
+                Page 1
+                <a href="#">></a>
             </div>
-            <div class="card">
-                <img src="img/destination/003/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/004/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/005/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/006/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/007/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/008/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-            <div class="card">
-                <img src="img/destination/009/main.jpg" alt="" class="card-img">
-                <div class="card-content">
-                    <p class="card-content-title">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="card-content-price">à partire de 684€</span>
-                    <a href="voyage001.php" class="card-content-seemore">Voir le Deal</a>
-                </div>
-            </div>
-        </div>
-        <div class="linkpage">
-            Page 1
-            <a href="#">></a>
         </div>
     </main>
 
--- voyage001.php
+++ voyage001.php
@@ -88,11 +88,113 @@
             </div>
         </div>
 
-        test
+        <div class="feature">
+            <table>
+                <tr>
+                    <td>Validité :</td>
+                    <td>3. juin 2020 - 23. sept. 2020</td>
+                </tr>
+                <tr>
+                    <td>Formules :</td>
+                    <td>Aucun repas inclus</td>
+                </tr>
+                <tr>
+                    <td>Au départ de :</td>
+                    <td>Bruxelles, Paris, Genève</td>
+                </tr>
+            </table>
 
+            <div class="separator"></div>
 
+            <p>Envie de vous prélasser au soleil dans un hôtel 5* au prix d'un 4. Il est situé en bord de mer et possède un spa où vous pourrez profiter de massages moyennant un supplément.</p>
+        </div>
 
+        <div class="presentation">
+            <h2>Présentation :</h2>
+            <p>
+                Située dans l'archipel des Baléares en Mer Méditerranée, l'île de Majorque est un paradis pour les amateurs de farniente. Ses 300 plages baignées par des eaux cristallines offrent une grande variété de paysages balnéaires : longues plages de sable fin ou criques sauvages et vierges, tout est réuni pour passer des vacances inoubliables.De nombreuses activités y sont proposées : randonnées (Sierra Tramuntana), golf, plongée, sports nautiques... Impossible de s'ennuyer! Du divertissement pour tous, petits et grands, avec de nombreux parcs d'attractions ou aquatiques (Marineland à Portals, Parc Aquatique à Magaluf, Aquacity à El Arenal...). Certaines zones de l'île proposent également de belles soirées festives dans des hauts lieux de la fête comme à Palma de Majorque ou à Magaluf. Derrière les remparts de Palma se cache la vieille ville et ses églises, palais et demeures seigneuriales aux superbes patios. La cathédrale de Palma se dresse sur un emplacement spectaculaire face à la baie et au port de pêche. Face à elle se dresse le palais de l'Almudaina. L'ancien palais arabe et résidence des monarques majorquins est aujourd'hui converti en musée. Sur le Paseo Sagrera tout proche se trouvent les halles qui comptent parmi les plus beaux exemples d'architecture gothique civile d'Espagne; cet ancien marché accueille désormais des expositions.
+                <br>
+                <br>L'<strong>Hôtel Club Jumbo Palia Puerto del Sol 3*sup</strong> (normes locales) est situé au sud-est de l'île, dans la zone touristique de Cala D´Or. À noter que Cala d'Or est une station touristique calme et peu animée située au Sud Est de l'île qui a su garder son charme de petit village d'antan. Elle est parcourue par une côte rocheuse, elle-même parsemée de petites criques aux eaux claires, qui caractérise tout le littoral Est de l'île. Vous y trouverez boutiques, restaurants et bars. Les plages les plus proches sont celles d'Es Forti (à environ 800 mètres) et de Cala D´Or (à environ 1,5 km) : elles sont accessibles par un petit train passant plusieurs fois par jour (payant). Boutiques, bars et restaurants à proximité. L'aéroport de Palma se situe à 65 km environ.
+                <br>Une navette pour la plage d'Es Forti est mise à disposition gratuitement. Horaires à consulter sur place.
+                <br>
+                <br>Profitez d'une formule tout compris pour des vacances décontractées et colorées. Du sport à la détente, de la culture au farniente… faites vos jeux ! Votre équipe d'animation Club Jumbo francophone rythme votre séjour à travers des activités sportives, ludiques et authentiques.
+                <br>Une ambiance festive, pour le plus grand bonheur des petits et des grands !
+            </p>
 
+            <br><br>
+
+            <h2>Logement :</h2>
+            <p>
+                Hôtel Club Jumbo Palia Puerto del Sol 3*sup dispose de 227 appartements rénovés en 2019, réparties sur 5 bâtiments de 3 étages, entourés de jardins méditerranéens.
+                <br>Les appartements 1 chambre (28 m²) sont équipés de :
+                <br>- salon avec 2 lits banquettes,
+                <br>- 1 chambre séparée (2 lits simples),
+                <br>- douche, sèche-cheveux,
+                <br>- climatisation,
+                <br>- mini-réfrigérateur,
+                <br>- télévision,
+                <br>- téléphone,
+                <br>- balcon ou terrasse aménagé vue piscine ou vue jardin.
+                <br>Capacité : 4 adultes (jusqu'à 6 adultes en appartement 2 chambres, de 45 à 60 m²).
+                <br>
+                <br>Avec supplément : wifi (20€/semaine), coffre-fort (15€/semaine + caution 10€).
+                <br>
+                <br>Répartition des appartements :
+                <br>- Appartements double : 2 adultes
+                <br>- Appartements triple : 2 adultes + 1 enfant (OU bébé) ou 3 adultes
+                <br>- Appartements quadruple : 2 adultes + 2 enfants (OU bébés) ou 3 adultes + 1 enfant (OU bébé) ou 4 adultes
+                <br>- Appartements quintuple : 2 adultes + 3 enfants (OU bébés) ou 3 adultes + 2 enfants (OU bébés) ou 4 adultes + 1 enfant (OU bébé) ou 5 adultes
+                <br>- Appartements sextuple : 2 adultes + 4 enfants (OU bébés) ou 3 adultes + 3 enfants (OU bébés) ou 4 adultes + 2 enfants (OU bébés) ou 5 adultes + 1 enfant (OU bébé) ou 6 adultes.
+                <br>
+                <br>A savoir :
+                <br>- Les appartements deux chambres sont adaptés à partir de 5 personnes.
+                <br>- L'hôtel ne dispose pas de chambres communicantes.
+                <br>- Possibilité de lit bébé (gratuit, sur demande et selon disponibilité).
+                <br>- Dans les appartements, l'eau du robinet est salée.
+            </p>
+        </div>
+
+        <div class="testimonials">
+            <h2>Les avis sur TripAdvisor :</h2>
+            <div class="container">
+                <div class="test-card">
+                    <span class="stars5">Par krystel172019 | Catégorie : En couple</span>
+                    <p>Hotel impeccable, calme, tres bien situé face a la marina, tres belle vue, tres propre, accueil tres chaleureux, belle restauration avec un grand choix de menu et employé tres respectueux. Hotel a por...</p>
+                </div>
+                <div class="separator"></div>
+                <div class="test-card">
+                    <span class="stars5">Par moniqueboucherand | Catégorie : Entre amis</span>
+                    <p>Hôtel refait à neuf avec de beaux appartements dans un beau cadre face à la médina. Personnel accueillant et compétent. Belles animations variées. Restauration proposant de bons et beaux buffets varié...+</p>
+                </div>
+                <div class="separator"></div>
+                <div class="test-card">
+                    <span class="stars5">Par Sylvama | Catégorie : En couple</span>
+                    <p>Nous avons passé un excellent séjour au sein de ce très beau complexe. Le personnel est d'une gentillesse remarquable, aimable et souriant malgré la charge de travail. Les appartements sont spacieux e...+</p>
+                </div>
+                <div class="separator"></div>
+                <div class="test-card">
+                    <span class="stars4">Par remy_oger | Catégorie : En couple</span>
+                    <p>Ma femme et moi avons passés un très bon séjour ! Étant en fin de saison nous avons été agréablement surpris du calme et de la disponibilité du personnel. Les points positifs : - Très belles chambres ...+</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="why">
+            <h2>Pourquoi choisir Simplon Voyage ?</h2>
+            <div class="container">
+                <div class="why-card">
+                    <span class="logosmile">Vous avez le choix de réserver en ligne, par téléphone ou dans l'une de nos agences</span>
+                </div>
+                <div class="separator"></div>
+                <div class="why-card">
+                    <span class="logocard">Vous avez le choix de réserver en ligne, par téléphone ou dans l'une de nos agences</span>
+                </div>
+                <div class="separator"></div>
+                <div class="why-card">
+                    <span class="logovip">Vous avez le choix de réserver en ligne, par téléphone ou dans l'une de nos agences</span>
+                </div>
+                </div>
+        </div>
 
 
     </main>
