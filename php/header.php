fb191b6bebbb3dc2c12cac5a36d6eeff0a904309
christophesd <christophe.sd@gmail.com>
2020-03-15 17:44:18 +0100

    gallery fiche produit

--- php/header.php
+++ php/header.php
@@ -1,46 +1,47 @@
-<header class="headerslide">
+<header class="gallery">
 
-    <div data-am-gallery>
-        <!-- Radio -->
-        <input type="radio" name="gallery" id="img-1" checked />
-        <input type="radio" name="gallery" id="img-2" />
-        <input type="radio" name="gallery" id="img-3" />
-        <!-- Images -->
-        <div class="images">
-            <div class="image" style="background-image: url(./img/destination/001/main.jpg);">
-                <a href="#main" class="gotobottom"></a>
-                <div class="slide-content">
-                    <span class="slide-title">Les meilleurs Deals</span>
-                    <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="slide-price">à partir de 684€</span>
-                    <a href="./voyage001.php" class="slide-seemore">Voir le Deal</a>
+    <!-- Radio -->
+    <input type="radio" name="gallery" id="img-1" checked />
+    <input type="radio" name="gallery" id="img-2" />
+    <input type="radio" name="gallery" id="img-3" />
+    <div class="headerslide">
+        <div data-am-gallery>
+            <!-- Images -->
+            <div class="images">
+                <div class="image" style="background-image: url(./img/destination/001/main.jpg);">
+                    <a href="#main" class="gotobottom"></a>
+                    <div class="slide-content">
+                        <span class="slide-title">Les meilleurs Deals</span>
+                        <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="slide-price">à partir de 684€</span>
+                        <a href="./voyage001.php" class="slide-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="image" style="background-image: url(./img/destination/002/main.jpg);">
+                    <a href="#main" class="gotobottom"></a>
+                    <div class="slide-content">
+                        <span class="slide-title">Les meilleurs Deals</span>
+                        <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="slide-price">à partir de 684€</span>
+                        <a href="#" class="slide-seemore">Voir le Deal</a>
+                    </div>
+                </div>
+                <div class="image" style="background-image: url(./img/destination/003/main.jpg);">
+                    <a href="#main" class="gotobottom"></a>
+                    <div class="slide-content">
+                        <span class="slide-title">Les meilleurs Deals</span>
+                        <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                        <span class="slide-price">à partir de 684€</span>
+                        <a href="#" class="slide-seemore">Voir le Deal</a>
+                    </div>
                 </div>
             </div>
-            <div class="image" style="background-image: url(./img/destination/002/main.jpg);">
-                <a href="#main" class="gotobottom"></a>
-                <div class="slide-content">
-                    <span class="slide-title">Les meilleurs Deals</span>
-                    <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="slide-price">à partir de 684€</span>
-                    <a href="#" class="slide-seemore">Voir le Deal</a>
-                </div>
+            <!-- Navigation -->
+            <div class="navigation">
+                <label class="dot" for="img-1"></label>
+                <label class="dot" for="img-2"></label>
+                <label class="dot" for="img-3"></label>
             </div>
-            <div class="image" style="background-image: url(./img/destination/003/main.jpg);">
-                <a href="#main" class="gotobottom"></a>
-                <div class="slide-content">
-                    <span class="slide-title">Les meilleurs Deals</span>
-                    <p class="slide-p">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                    <span class="slide-price">à partir de 684€</span>
-                    <a href="#" class="slide-seemore">Voir le Deal</a>
-                </div>
-            </div>
-        </div>
-        <!-- Navigation -->
-        <div class="navigation">
-            <label class="dot" for="img-1"></label>
-            <label class="dot" for="img-2"></label>
-            <label class="dot" for="img-3"></label>
         </div>
     </div>
-
 </header>
--- voyage001.php
+++ voyage001.php
@@ -28,40 +28,67 @@
 
     <main id="main">
         <h2>Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</h2>
-        <header class="galleryslide">
-            <div data-am-gallery>
-                <!-- Radio -->
-                <input type="radio" name="gallery" id="img-1" checked />
-                <input type="radio" name="gallery" id="img-2" />
-                <input type="radio" name="gallery" id="img-3" />
-                <!-- Images -->
-                <div class="images">
-                    <div class="image" style="background-image: url(./img/destination/001/main.jpg);">
-                        <div class="slide-content blackshadow">
-                            <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+        <div class="gallery">
+            <!-- Radio -->
+            <input type="radio" name="gallery" id="img-1" checked />
+            <input type="radio" name="gallery" id="img-2" />
+            <input type="radio" name="gallery" id="img-3" />
+            <div class="galleryslide">
+                <div data-am-gallery>
+                    <!-- Images -->
+                    <div class="images">
+                        <div class="image" style="background-image: url(./img/destination/001/main.jpg);">
+                            <!-- <label class="left" for="img-3"></label>
+                            <label class="right" for="img-2"></label> -->
+                            <div class="slide-content blackshadow">
+                                <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                            </div>
+                        </div>
+                        <div class="image" style="background-image: url(./img/destination/001/001.jpg);">
+                            <!-- <label class="left" for="img-1"></label>
+                            <label class="right" for="img-3"></label> -->
+                            <div class="slide-content blackshadow">
+                                <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                            </div>
+                        </div>
+                        <div class="image" style="background-image: url(./img/destination/001/002.jpg);">
+                            <!-- <label class="left" for="img-2"></label>
+                            <label class="right" for="img-1"></label> -->
+                            <div class="slide-content blackshadow">
+                                <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="image" style="background-image: url(./img/destination/002/main.jpg);">
-                        <div class="slide-content blackshadow">
-                            <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
+                    <!-- Navigation -->
+                    <div class="navigation">
+                        <label class="dot" for="img-1"></label>
+                        <label class="dot" for="img-2"></label>
+                        <label class="dot" for="img-3"></label>
+                    </div>
+                    <div class="leftright">
+                        <div class="slide">
+                            <label class="left" for="img-3"></label>
+                            <label class="right" for="img-2"></label>
+                        </div>
+                        <div class="slide">
+                            <label class="left" for="img-1"></label>
+                            <label class="right" for="img-3"></label>
+                        </div>
+                        <div class="slide">
+                            <label class="left" for="img-2"></label>
+                            <label class="right" for="img-1"></label>
                         </div>
                     </div>
-                    <div class="image" style="background-image: url(./img/destination/003/main.jpg);">
-                        <div class="slide-content blackshadow">
-                            <p class="slide-p-des">Vacances 4* à Miami : 7 jours à partir de 684€, hôtel 4* avec piscine et vol A/R inclus</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Navigation -->
-                <div class="navigation">
-                    <label class="dot" for="img-1"></label>
-                    <label class="dot" for="img-2"></label>
-                    <label class="dot" for="img-3"></label>
                 </div>
             </div>
+            <div class="mini">
+                    <label for="img-1" style="background-image: url(./img/destination/001/main.jpg);"></label>
+                    <label for="img-2" style="background-image: url(./img/destination/001/001.jpg);"></label>
+                    <label for="img-3" style="background-image: url(./img/destination/001/002.jpg);"></label>
+            </div>
+        </div>
 
-        </header>
-        
+        test
 
 
 
